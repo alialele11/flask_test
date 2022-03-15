c248e6f04cf827d0abc9e0c943cf906e64f691e0
A. Lebedeva <Alina.Lebedeva29@yandex.ru>
2022-03-15 21:45:27 +0300

    lessons 6-7: flash, login, error

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-from flask import Flask
-from flask import render_template
-from flask import url_for, request
+from flask import Flask, session, redirect
+from flask import render_template, flash, get_flashed_messages
+from flask import url_for, request, abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'asdsfdsdgszkll'
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -21,13 +22,34 @@ def about():
 
 @app.route("/profile/<username>")
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
     return "Пользователь: {}".format(username)
 
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        print(request.form)
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
     return render_template('contact.html', title="Обратная связь", menu=menu)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == "POST" and request.form['username'] == 'selfedu' and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
