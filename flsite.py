fb263158fd1e3108fb1e248f635090b0bc21dca6
A. Lebedeva <Alina.Lebedeva29@yandex.ru>
2022-03-18 23:21:26 +0300

    lesson 9: work with db

--- flsite.py
+++ flsite.py
@@ -1,6 +1,8 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g, flash
+from FDataBase import FDataBase
+
 
 # Конфигурация
 DATABASE = '/tmp/flsite.db'
@@ -12,10 +14,12 @@ app.config.from_object(__name__)
 
 app.config.update((dict(DATABASE=os.path.join(app.root_path, 'flsite.db'))))
 
+
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def create_db():
     db = connect_db()
@@ -24,17 +28,51 @@ def create_db():
     db.commit()
     db.close()
 
+
 def get_db():
     if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
     return g.link_db
 
+
 @app.route("/")
 def index():
     db = get_db()
-    return render_template('index.html', menu = [])
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnnonce())
+
 
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'link_db'):
         g.link_db.close()
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
